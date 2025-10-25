40c7422f6b5b2466b82a288eaea5295f3c8b88b3
yutijang <yutijang@gmail.com>
2025-10-25 17:05:26 +0700

    y

--- vcpkg-triplets/x64-linux-dynamic.cmake
+++ vcpkg-triplets/x64-linux-dynamic.cmake
@@ -1,3 +1,6 @@
-include(${VCPKG_ROOT}/triplets/x64-linux.cmake)
+get_filename_component(_VCPKG_TRIPLET_DIR "${CMAKE_CURRENT_LIST_DIR}" ABSOLUTE)
+
+include("${_VCPKG_TRIPLET_DIR}/../x64-linux.cmake")
+
 set(VCPKG_LIBRARY_LINKAGE dynamic)
 set(VCPKG_CRT_LINKAGE dynamic)
