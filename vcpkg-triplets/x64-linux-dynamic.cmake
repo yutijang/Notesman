13371a43402553f334f7ab13f7023aa4be68157e
yutijang <yutijang@gmail.com>
2025-10-25 16:58:24 +0700

    u

--- vcpkg-triplets/x64-linux-dynamic.cmake
+++ vcpkg-triplets/x64-linux-dynamic.cmake
@@ -1,8 +1,3 @@
-set(VCPKG_TARGET_ARCHITECTURE x64)
-set(VCPKG_CRT_LINKAGE dynamic)
+include(${VCPKG_ROOT}/triplets/x64-linux.cmake)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
-
-set(VCPKG_CMAKE_SYSTEM_NAME Linux)
-
-# Dùng clang thay vì gcc
-set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "/usr/share/cmake/clang-toolchain.cmake" CACHE STRING "")
+set(VCPKG_CRT_LINKAGE dynamic)
