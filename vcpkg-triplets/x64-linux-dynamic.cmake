f5b9bc1a14b46ad2710058f45c010a0e919c0cd6
yutijang <yutijang@gmail.com>
2025-10-25 17:12:25 +0700

    u

--- vcpkg-triplets/x64-linux-dynamic.cmake
+++ vcpkg-triplets/x64-linux-dynamic.cmake
@@ -1,6 +1,10 @@
-get_filename_component(_VCPKG_TRIPLET_DIR "${CMAKE_CURRENT_LIST_DIR}" ABSOLUTE)
+# Kế thừa triplet gốc của vcpkg
+include(${VCPKG_ROOT}/triplets/x64-linux.cmake)
 
-include("${_VCPKG_TRIPLET_DIR}/../x64-linux.cmake")
-
-set(VCPKG_LIBRARY_LINKAGE dynamic)
+set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+
+# Dùng clang thay vì gcc
+set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "/usr/share/cmake/clang-toolchain.cmake" CACHE STRING "")
