a94169fe91085fd44780e4680c5544001e1032ee
yutijang <yutijang@gmail.com>
2026-01-04 23:05:51 +0700

    test updater linux, ignored logger from updater binary

--- apps/updater/updater_linux.cpp
+++ apps/updater/updater_linux.cpp
@@ -8,13 +8,9 @@
 #include <unistd.h>
 #include <sys/stat.h>
 
-#include "Logger.hpp"
-
 namespace fs = std::filesystem;
 
 int main(int argc, char** argv) {
-    Log::init("updater");
-
     if (argc < 3) { return 1; }
 
     std::error_code ec;
